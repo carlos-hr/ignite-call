ad5ae764879140e1576ae12244adb4d98488b232
carlos-hr <carlos.henrique17.chr@gmail.com>
2022-12-28 21:47:08 -0300

    feat: setup styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({ weight: ['400', '500', '700'] })
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
