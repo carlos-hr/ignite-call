beaf722536ee18df6752ee76c56967e7ff55b500
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-01-26 09:12:09 -0300

    feat: configuring page titles

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,10 +1,11 @@
 import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
 import { Roboto } from '@next/font/google'
-import { globalStyles } from '../styles/global'
-import '../lib/dayjs'
-import { queryClient } from '../lib/react-query'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { DefaultSeo } from 'next-seo'
+import { globalStyles } from '../styles/global'
+import { queryClient } from '../lib/react-query'
+import '../lib/dayjs'
 
 const roboto = Roboto({ weight: ['400', '500', '700'] })
 globalStyles()
@@ -16,6 +17,14 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.url.ie/',
+            siteName: 'SiteName',
+          }}
+        />
         <main className={roboto.className}>
           <Component {...pageProps} />
         </main>
